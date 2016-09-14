b6e58fc158a96bddac0f3c75c25682874d5f6919
CallumOakey <Callum@ertmssolutions.com>
2016-09-14 04:10:49 +0200

    Tests + text file as input

--- MarkovProcess/MarkovProcess/src/Main.hs
+++ MarkovProcess/MarkovProcess/src/Main.hs
@@ -18,23 +18,28 @@ module Main (
 
 import System.IO
 import Markov.Functions
+import Test.HUnit
 
 main :: IO ()
 main = do
-    text <- getContents
-    let wordsList = words text
+    runTestTT tests
+    handle <- openFile "ab.txt" ReadMode
+    trainingText <- hGetContents handle 
+    let wordsList = words trainingText
     print wordsList
-    let textZip = zip (init wordsList) (tail wordsList)
-    print textZip
-    let wordCouplesList = markovWordCoupleList textZip []
+    let wordsZip = zip (init wordsList) (tail wordsList)
+    print wordsZip
+    let wordCouplesList = markovWordCoupleList wordsZip []
     print wordCouplesList
     let markovSet = markov wordsList wordCouplesList
     print markovSet
     return ()
 
-testText = ["a", "b", "a", "a", "b", "b", "a", "b"]
-zipTestText = zip (init testText) (tail testText)
+text = "a b a a b b a b"
+textWords = words text
+textZip = zip (init textWords) (tail textWords)
 
-testMarkovResList = markovResList testText ("a", "b")
-testMarkovWordCoupleList = markovWordCoupleList zipTestText []
-testMarkov = markov testText testMarkovWordCoupleList
+test1 =  markovWordCoupleList textZip [] ~=? [("a","b"), ("b","a"), ("a","a"), ("b","b")]
+test2 =  markov textWords (markovWordCoupleList textZip []) ~=? [(("a","b"),["a","b"]),(("b","a"),["a","b"]),(("a","a"),["b"]),(("b","b"),["a"])]
+
+tests = TestList [ TestLabel "Test1" test1, TestLabel "Test2" test2  ]
--- MarkovProcess/MarkovProcess/test/Main.hs
+++ MarkovProcess/MarkovProcess/test/Main.hs
@@ -16,8 +16,22 @@ module Main (
     main
 ) where
 
+import Test.HUnit
+import Markov.Functions
+
+text = "a b a a b b a b"
+wordsList = words text
+textZip = zip (init wordsList) (tail wordsList)
+
+test1 =  markovWordCoupleList textZip [] ~=? [("a","b"), ("b","a"), ("a","a"), ("b","b")]
+test2 =  markov wordsList wordCouplesList ~=? [(("a","b"),["a","b"]),(("b","a"),["a","b"]),(("a","a"),["b"]),(("b","b"),["a"])]
+
+tests = TestList [ TestLabel "Test1" test1, TestLabel "Test2" test2  ]
+
+-- Entry point of the tests
 main :: IO ()
 main = do
+    runTestTT tests
     return ()
 
 
