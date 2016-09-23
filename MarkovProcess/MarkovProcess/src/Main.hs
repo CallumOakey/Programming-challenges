840a18490087cbcbee6520cec4c40c313fc12951
Callum <Callum@ertmssolutions.com>
2016-09-23 17:48:02 +0200

    Changed source text.

--- MarkovProcess/MarkovProcess/src/Main.hs
+++ MarkovProcess/MarkovProcess/src/Main.hs
@@ -19,11 +19,12 @@ module Main (
 import System.IO
 import Markov.Functions
 import Test.HUnit
+import System.Random
 
 main :: IO ()
 main = do
     runTestTT tests
-    handle <- openFile "ab.txt" ReadMode
+    handle <- openFile "source.txt" ReadMode
     trainingText <- hGetContents handle 
     let wordsList = words trainingText
         wordsZip = zip (init wordsList) (tail wordsList)
@@ -42,5 +43,6 @@ test1 =  markovWordCoupleList textZip [] ~=? [("a","b"), ("b","a"), ("a","a"), (
 test2 =  markov textWords (markovWordCoupleList textZip []) ~=? [(("a","b"),["a","b"]),(("b","a"),["a","b"]),(("a","a"),["b"]),(("b","b"),["a"])]
 test3 =  countResults ["a","b","b","a","a","b","a","a","b"] [] ~=? [("a",5),("b",4)]
 test4 =  countMarkovSetResults [(("a","b"),["a","b"]),(("b","a"),["a","b"]),(("a","a"),["b"]),(("b","b"),["a"])] ~=? [(("a","b"),[("a",1),("b",1)]),(("b","a"),[("a",1),("b",1)]),(("a","a"),[("b",1)]),(("b","b"),[("a",1)])]
+test5 =  randWordCouple [(("a","b"),[("a",1),("b",1)]),(("b","a"),[("a",1),("b",1)]),(("a","a"),[("b",1)]),(("b","b"),[("a",1)])] (mkStdGen 50222222000)
 
 tests = TestList [ TestLabel "Test1" test1, TestLabel "Test2" test2, TestLabel "Test3" test3, TestLabel "Test4" test4 ]
